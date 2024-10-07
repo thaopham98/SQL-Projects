6beae4057d8204abac957e7951aa0f4630965c91
thaopham98 <79811818+thaopham98@users.noreply.github.com>
2024-10-07 13:01:56 -0700

    Added  folder to  folder in

---
+++ MSBA230/tables/W3_JOIN.sql
@@ -0,0 +1,76 @@
+-- drop table Customer;
+-- drop database week3_JOIN;
+-- CREATE database week3_JOIN;
+-- Create table Customer (
+-- CustNo varchar(255), 
+-- CustFirstName varchar(255), 
+-- CustLastName varchar(255), 
+-- CustCity varchar(255), 
+-- CustState varchar(255),
+-- CustZip varchar(255),
+-- CustBal varchar(255));
+
+-- insert into Customer (CustNo, 
+-- 						CustFirstName, 
+-- 						CustLastName, 
+-- 						CustCity, 
+-- 						CustState,
+-- 						CustZip,
+-- 						CustBal)
+-- values 
+-- 	('C0954327', 'Sheri', 'Gordon', 'Littleton', 'CO', '80129-5543', '$230.00'),
+--     ('C1010398', 'Jim', 'Glussman', 'Denver', 'CO', '80111-0033', '$200.00'),
+--     ('C2388597', 'Beth', 'Taylor', 'Seattle', 'WA','98103-1121', '$500.00'),
+--     ('C3340959', 'Betty', 'Wise', 'Seattle', 'WA', '98178-3311', '$200.00'),
+--     ('C3499503', 'Bob', 'Mann', 'Monroe', 'WA', '98013-1095', '$0.00'),
+--     ('C8543321','Ron', 'Thompson', 'Renton', 'WA', '98666-1289', '$85.00')
+--     ;
+
+-- create table Employee ( 
+-- 	EmpNo varchar(255),
+--     EmpFirstName varchar(255),
+--     EmpLastName varchar(255),
+--     EmpPhone varchar(255),
+--     EmpEmail varchar(255)
+-- );
+
+-- Insert into Employee (EmpNo,
+--     EmpFirstName,
+--     EmpLastName,
+--     EmpPhone,
+--     EmpEmail)
+-- Values 
+-- 	('E1329594', 'Landi', 'Santos', '(303) 789-1234', 'LSantos@bigco.com'),
+--     ('E8544399', 'Joe', 'Jenkins', '(303) 221-9875', 'JJenkins@bigco.com'),
+--     ('E8843211', 'Amy', 'Tang', '(303) 556-4321', 'ATang@bigco.com'),
+--     ('E9345771', 'Colin', 'White', '(303) 221-4453', 'CWhite@bigco.com'),
+--     ('E9884325', 'Thomas', 'Johnson', '(303) 556-9987', 'TJohnson@bigco.com'),
+--     ('E9954302', 'Mary', 'Hill', '(303) 556-9871', 'MHill@bigco.com');
+
+
+-- create table OrderTbl ( 
+-- 	OrdNo varchar(255),
+--     OrdDate varchar(255),
+--     CustNo varchar(255),
+--     EmpNo varchar(255)
+-- );
+
+-- Insert into OrderTbl (OrdNo,
+--     OrdDate,
+--     CustNo,
+--     EmpNo)
+-- Values 
+-- 	('O1116324', '01/23/2017', 'C0954327', 'E8544399'),
+--     ('O2334661', '01/14/2017', 'C0954327', 'E1329594'),
+--     ('O3331222', '01/13/2017', 'C1010398', null),
+--     ('O2233457', '01/12/2017', 'C2388597', 'E9884325'),
+--     ('O4714645', '01/11/2017', 'C2388597', 'E1329594'),
+--     ('O5511365', '01/22/2017', 'C3340959', 'E9884325'),
+--     ('O7989497', '01/16/2017', 'C3499503', 'E9345771'),
+--     ('O1656777', '02/11/2017', 'C8543321', null),
+--     ('O7959898', '02/19/2017', 'C8543321', 'E8544399');
+
+-- SELECT * FROM Customer;
+-- SELECT * FROM OrderTbl;
+-- SELECT * FROM Employee;
+-- WHERE CustNo NOT IN (SELECT CustNo FROM OrderTbl);
